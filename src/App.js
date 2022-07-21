c2f105e30da74873e77b9437b1c1e10a58b202d0
hallibentley <hallibentley@gmail.com>
2022-07-21 07:26:58 -0400

    added tests

--- src/App.js
+++ src/App.js
@@ -3,13 +3,41 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
+
+  state = {
+    events: [],
+    locations: []
+  }
+
+  componentDidMount() {
+    getEvents().then((events) => {
+      this.setState({ events, locations: extractLocations(events) });
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
