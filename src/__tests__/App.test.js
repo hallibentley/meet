9efa65834a3abc7ff7cb20fe95129cb8b0e5842c
hallibentley <hallibentley@gmail.com>
2022-07-25 09:45:10 -0400

    mount and unmount for app.test.js tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -34,30 +34,26 @@ describe('<App /> component', () => {
 
 
 describe('<App /> integration', () => {
-
   let AppWrapper;
 
-  beforeAll(() => {
-    AppWrapper = mount(<App />);
-  });
-
-  afterAll(() => {
-    AppWrapper.unmount();
-  });
-
   test('App passes "events" state as a prop to EventList', () => {
+    AppWrapper = mount(<App />);
     const AppEventsState = AppWrapper.state('events');
     expect(AppEventsState).not.toEqual(undefined);
     expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
   });
 
   test('App passes "locations" state as a prop to CitySearch', () => {
+    AppWrapper = mount(<App />);
     const AppLocationsState = AppWrapper.state('locations');
     expect(AppLocationsState).not.toEqual(undefined);
     expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+    AppWrapper.unmount();
   });
 
   test('get list of events matching the city selected by the user', async () => {
+    AppWrapper = mount(<App />);
     const CitySearchWrapper = AppWrapper.find(CitySearch);
     const locations = extractLocations(mockData);
     CitySearchWrapper.setState({ suggestions: locations });
@@ -68,13 +64,16 @@ describe('<App /> integration', () => {
     const allEvents = await getEvents();
     const eventsToShow = allEvents.filter(event => event.location === selectedCity);
     expect(AppWrapper.state('events')).toEqual(eventsToShow);
+    AppWrapper.unmount();
   });
 
   test('get list of all events when user selects "See all cities"', async () => {
+    AppWrapper = mount(<App />);
     const suggestionItems = AppWrapper.find(CitySearch).find('.suggestions li');
     await suggestionItems.at(suggestionItems.length - 1).simulate('click');
     const allEvents = await getEvents();
     expect(AppWrapper.state('events')).toEqual(allEvents);
+    AppWrapper.unmount();
   });
 
 });
