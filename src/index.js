3ccb93196a20b2c5a15431ca3070d598c8a1d36d
hallibentley <hallibentley@gmail.com>
2022-07-28 10:33:00 -0400

    added atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('1bf29ff139f34699bc9d0900468dbcd8').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
