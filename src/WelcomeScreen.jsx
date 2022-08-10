208223d099c9f8cbf120c6b68484b788bed18885
hallibentley <hallibentley@gmail.com>
2022-08-10 10:16:09 -0400

    welcome screen css

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,10 +4,10 @@ import "./WelcomeScreen.css";
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
-      <h1>Welcome to the Meet app</h1>
-      <h4>
-        Log in to see upcoming events around the world for full-stack developers
-      </h4>
+      <h1>Welcome to the Meet App!</h1>
+      <p>
+        Log in to see upcoming events around the world for full-stack developers.
+      </p>
       <div className="button_cont" align="center">
         <div class="google-btn">
           <div class="google-icon-wrapper">
